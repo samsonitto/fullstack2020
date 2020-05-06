7fc71504eba812b691920c6cff2fde3527c13f35
M3156 <m3156@student.jamk.fi>
2020-05-06 09:48:32 +0300

    1.14 done

--- osa1/anecdotes/src/index.js
+++ osa1/anecdotes/src/index.js
@@ -2,10 +2,30 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = (props) => <button onClick={props.handleClick} >{props.text}</button>
+const Header = (props) => <h1>{props.text}</h1>
+const TextLine = (props) => <p>{props.text}</p>
+
+const MostVotes = (props) => {
+  if(!props.votes) {
+    return (
+      <></>
+    )
+  }
+
+  return (
+    <>
+      <Header text={'Anecdote with most votes'}/>
+      <TextLine text={props.mostVotesAnecdote} />
+      <TextLine text={props.textVoteAmount} />
+    </>
+  )
+}
 
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(Math.floor(Math.random() * 6))
   const [points, setPoints] = useState(Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0));
+  const [max, setMax] = useState(0)
+  const [votes, setVotes] = useState(false)
 
   const nextAnecdote = () => {
     setSelected(Math.floor(Math.random() * 6))
@@ -15,16 +35,40 @@ const App = (props) => {
     const copy =  [...points] 
     copy[selected] += 1
     setPoints(copy)
+
+    setMax(indexOfMax(copy))
+    setVotes(true)
+  }
+
+  const indexOfMax = (arr) => {
+    if (arr.length === 0) {
+      return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
   }
 
   let voteText = points[selected] === 1 ? 'vote' : 'votes'
+  let mostText = "Has " + points[max] + " votes"
 
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
+      <Header text={'Anecdote of the Day'} />
+      <TextLine text={anecdotes[selected]} />
       <p>Has {points[selected]} {voteText}</p>
       <Button handleClick={voteAnecdote} text={'Vote'} />
       <Button handleClick={nextAnecdote} text={'Next Anecdote'} />
+      <MostVotes mostVotesAnecdote={anecdotes[max]} textVoteAmount={mostText} votes={votes}/>
     </div>
   )
 }
