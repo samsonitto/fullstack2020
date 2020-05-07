d5b7b4f2eb48cc9b32dc6ac76c47dfd7e2636d79
M3156 <m3156@student.jamk.fi>
2020-05-07 10:52:18 +0300

    2.11 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react'
-import PlainText from './components/PlainText'
+import React, { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Filter from './components/Filter'
 import AddNewContact from './components/AddNewContact'
 import Contacts from './components/Contacts'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ personsToShow, setPersonsToShow] = useState(persons)
+  const [ dataLoaded, setDataLoaded ] = useState(false)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setPersonsToShow(response.data)
+      })
+    
+  }, [])
+  console.log('render', persons.length, 'persons');
+  
 
   const handleAddClick = (e) => {
     e.preventDefault()
